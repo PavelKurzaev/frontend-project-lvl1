96010c209be04922bdc85ba4eb5ef7cda4b9959c
Pavel Kurzaev <espakurzaev@gmail.com>
2022-05-02 17:16:50 +0200

    add gcd

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,11 @@
 import readlineSync from 'readline-sync';
 
-const func = () => {
+const func = (message) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  console.log(message);
+  return name;
 };
 
 export default func;
---
+++ src/playCalc.js
@@ -0,0 +1,43 @@
+import readlineSync from 'readline-sync';
+import getUserName from '../src/cli.js';
+
+const MAX_NUMBER = 100;
+
+const playCalc = () => {
+    const name = getUserName('What is the result of the expression?');
+
+    let i = 3;
+    const operArray = ['+', '-', '*', '/'];
+    do {
+        const number1 = Math.trunc(Math.random() * MAX_NUMBER);
+        const number2 = Math.trunc(Math.random() * MAX_NUMBER);
+        const operation = Math.trunc(Math.random() * operArray.length - 1);
+        let correctAnswer = 0;
+        let answer = 0;
+        switch (operation) {
+            case 0:
+                correctAnswer = number1 + number2;
+                break;
+            case 1:
+                correctAnswer = number1 - number2;
+                break;
+            case 2:
+                correctAnswer = number1 * number2;
+                break;
+        }
+        const operSymbol = operArray[operation];
+        answer = Number(readlineSync.question(`Question: ${number1} ${operSymbol} ${number2}\nYour answer: `));
+        const correct = answer === correctAnswer;
+        if (!correct) {
+            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+            console.log('Let\'s try again, Sam!');
+            return false;
+        }
+        console.log('Correct!');
+        i -= 1;
+    } while (i > 0);
+    console.log(`Congratulations, ${name}!`);
+    return true;
+};
+
+export default playCalc;
